224a9f211ecd477f7134d83d20caf397515a2002
CJB121 <55166574+CJB121@users.noreply.github.com>
2020-02-04 19:07:03 -0500

    Update to Program.cs
    Added Daringer

--- GriseldaExample/GriseldaExample/Program.cs
+++ GriseldaExample/GriseldaExample/Program.cs
@@ -9,6 +9,7 @@ namespace GriseldaExample
             GriseldaMember BennyTheButcher = new GriseldaMember("Benny The Butcher");
             GriseldaMember WestSideGunn = new GriseldaMember("Westside Gunn");
             GriseldaMember BConwayTheMachine = new GriseldaMember("Conway The Machine");
+            GriseldaMember Daringer = new GriseldaMember("Daringer");
         }
     }
 }
